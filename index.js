55cf99b2576aa17fc0869d616972df7df5e2b172
Panagiotis Pagonis <pan.m.pagonis@gmail.com>
2023-08-23 12:03:12 +0300

    Finished Project

--- index.js
+++ index.js
@@ -44,10 +44,15 @@ const userInfo = document.getElementById("Info-Box");
 const greenCursor = document.getElementById("cursor");
 const screenText = document.getElementById("specifics");
 const myspan= document.querySelector('.myspan');
+const buttonsSection = document.getElementById("buttonSection");
 const buttons = document.querySelectorAll('.buttons');
 const myImage = document.getElementById("eikona");
 const myInfo = document.getElementById("Infolist");
 const listItems = userInfo.querySelectorAll('p');
+const exitingApp = document.getElementById("exitingStage");
+const loadingBar = document.getElementById("loadingBar");
+const SignOutMessage  = document.getElementById("SignOut");
+const thankyou = document.getElementById("Thanks");
 let counttime;
 
 welcomeBtn.addEventListener('click', async function() {
@@ -96,7 +101,7 @@ welcomeBtn.addEventListener('click', async function() {
                 greenCursor.style.display = "none"; 
                 console.log("cursor is gone");
                 myspan.style.display = "inline-block";
-                animateText(newString);
+                animateText(screenText, newString);
                 counttime = newString.length * 20;
                 await RevealButtons(buttons, counttime);
             },1100)
@@ -153,13 +158,107 @@ welcomeBtn.addEventListener('click', async function() {
 
 */
 
+
+/*
 const ekpaideusi = ["Αριστούχος Απόφοιτος Λυκείου με βαθμό απολυτηρίου 18.1   (2013)", 
                     "Απόφοιτος Τμήματος Μαθηματικών Αθήνας   (2020)", 
                     " Δευτεροετής Φοιτητής Μεταπτυχιακού Προγράμματος Πληροφορικής του ΕΑΠ με τίτλο «Εξειδίκευση στα πληροφοριακά συστήματα»   (2022 - )"];
+*/
 
-const ekpaideusi2 = ["Αριστούχος Απόφοιτος Λυκείου με βαθμό απολυτηρίου 18.1   (2013)\nΑπόφοιτος Τμήματος Μαθηματικών Αθήνας   (2020)\nΔευτεροετής Φοιτητής Μεταπτυχιακού Προγράμματος Πληροφορικής του ΕΑΠ με τίτλο «Εξειδίκευση στα πληροφοριακά συστήματα»   (2022 - )"];
+const ekpaideusi =  "\n§ ΕΚΠΑΙΔΕΥΣΗ §\n\n\nΑριστούχος Απόφοιτος Λυκείου με βαθμό απολυτηρίου 18.1   (2013)\n\nΑπόφοιτος Τμήματος Μαθηματικών Αθήνας   (2020)\n\nΔευτεροετής Φοιτητής Μεταπτυχιακού Προγράμματος Πληροφορικής του ΕΑΠ με τίτλο «Εξειδίκευση στα πληροφοριακά συστήματα»   (2022 - )\n\n\n\n§ ΞΕΝΕΣ ΓΛΩΣΣΕΣ §\n\n\nΑγγλικά   (First Certificate)\n\nΓερμανικά   (Πτυχίο Β1)\n";
+
+const empeiria = "\n§ ΕΠΑΓΓΕΛΜΑΤΙΚΗ ΕΜΠΕΙΡΙΑ §\n\n\nΚαθηγητής Μαθηματικών στο φροντιστήριο «Ρόμβος»   (2014 - 2015)\n\nΚαθηγητής Μαθηματικών και Πληροφορικής Γυμνασίου, Γενικού Λυκείου και ΕΠΑΛ στον όμιλο «Πουκαμισάς»   (2021 - 2023)\n\nΚαθηγητής Μαθηματικών και Πληροφορικής Γυμνασίου, Γενικού Λυκείου και ΕΠΑΛ στο φροντιστήριο «Βολίδης»   (2023 - )\n\nΚαθηγητής Πληροφορικής Γενικού Λυκείου και ΕΠΑΛ στο φροντιστήριο «Μαργαρώνης»   (2023 - )\n\nΠαράδοση ιδιαιτέρων μαθημάτων Mαθηματικών και Πληροφορικής σε μαθητές Γυμνασίου και Λυκείου (2014 - )\n";
+
+const drastiriotites = "\n§ ΔΡΑΣΤΗΡΙΟΤΗΤΕΣ – ΕΘΕΛΟΝΤΙΣΜΟΣ §\n\n\nΚαθηγητής Μαθηματικών στο «Κοινωνικό Φροντιστήριο Δήμου Δάφνης»   (2017 - 2019)\n\nΟμαδάρχης στην κατασκήνωση «Summer Fun»   (2015)\n";
+
+const prosonta = "\n§ ΔΕΞΙΟΤΗΤΕΣ - ΠΡΟΣΟΝΤΑ §\n\n\nΠροθυμία, γρήγορη προσαρμογή και ευελιξία στις εκάστοτε εργασιακές απαιτήσεις με μεγάλη απόδοση και αποτελεσματικότητα\n\nΕυγένεια και μεγάλη ευχέρεια στην επικοινωνία\n";
+
+const quitingApp = "\n § ΕΞΟΔΟΣ § \n\n\nΜεταφέρεστε εκτός Matrix CV..."
 
 
+buttons[1].addEventListener('click', async () => {
+    let newString = ekpaideusi;
+    screenText.innerHTML = "";
+    greenCursor.style.display = "inline-block";
+    setTimeout(async function(){
+        greenCursor.style.display = "none"; 
+        console.log("cursor is gone");
+        myspan.style.display = "inline-block";
+        animateText(screenText, newString, 10);
+    },1100)
+})
+
+
+buttons[2].addEventListener('click', async () => {
+    let newString = empeiria;
+    screenText.innerHTML = "";
+    greenCursor.style.display = "inline-block";
+    setTimeout(async function(){
+        greenCursor.style.display = "none"; 
+        console.log("cursor is gone");
+        myspan.style.display = "inline-block";
+        animateText(screenText, newString, 10);
+    },1100)
+})
+
+buttons[3].addEventListener('click', async () => {
+    let newString = drastiriotites;
+    screenText.innerHTML = "";
+    greenCursor.style.display = "inline-block";
+    setTimeout(async function(){
+        greenCursor.style.display = "none"; 
+        console.log("cursor is gone");
+        myspan.style.display = "inline-block";
+        animateText(screenText, newString, 10);
+    },1100)
+})
+
+buttons[4].addEventListener('click', async () => {
+    let newString = prosonta;
+    screenText.innerHTML = "";
+    greenCursor.style.display = "inline-block";
+    setTimeout(async function(){
+        greenCursor.style.display = "none"; 
+        console.log("cursor is gone");
+        myspan.style.display = "inline-block";
+        animateText(screenText, newString, 10);
+    },1100)
+})
+
+buttons[5].addEventListener('click', async () => {
+    let newString = quitingApp;
+    screenText.innerHTML = "";
+    greenCursor.style.display = "inline-block";
+    setTimeout(async function(){
+        greenCursor.style.display = "none"; 
+        console.log("cursor is gone");
+        myspan.style.display = "inline-block";
+        counttime = newString.length * 10 + 1000;
+        animateText(screenText, newString, 10);
+        setTimeout(async function(){
+            detailsBox.style.display = "none";
+            userInfo.style.display = "none";
+            buttonsSection.style.display = "none";
+            exitingApp.style.display = "flex";
+            SignOutMessage.innerHTML = SignOutMessage.innerHTML.slice(0, -3);
+            animateTextWithoutCursor("...", 500, SignOutMessage.innerHTML);
+            await LoadBar(loadingBar).finished;
+            setTimeout(async function(){
+                thankyou.style.color = "white";
+            }, 2000);
+        }, counttime);
+    },1100)
+})
+
+
+
+/* 
+
+ΞΕΝΕΣ ΓΛΩΣΣΕΣ\n\nΑγγλικά   (First Certificate)\nΓερμανικά   (Πτυχίο Β1)
+
+
+
+*/
 
 
 
@@ -304,25 +403,47 @@ function MoveRight(element, time){
 }
 
 
-function animateText(text) {
-    screenText.innerHTML = ""; // Καθαρίστε το κείμενο
-    myspan.innerHTML = "";
-    screenText.appendChild(myspan);
+function animateText(element, text, delayBetweenLetters = 20, initialText = "") {
+    element.innerHTML = initialText // Καθαρίστε το κείμενο
+    myspan.innerHTML = " ";
+    element.appendChild(myspan);
     let index = 0;
 
 
     function addNextChar() {
         if (index < text.length) {
-            screenText.removeChild(myspan);
+            element.removeChild(myspan);
             if (text[index] === '\n') {
-                screenText.innerHTML += '<br>'; // Προσθέστε <br> για αλλαγή γραμμής
+                element.innerHTML += '<br>'; // Προσθέστε <br> για αλλαγή γραμμής
             } else {
-                screenText.innerHTML += text[index] ;
+                element.innerHTML += text[index] ;
             }
             index++;
-            setTimeout(addNextChar, 20); // Καλέστε τη συνάρτηση ξανά μετά από το χρονικό διάστημα
+            setTimeout(addNextChar, delayBetweenLetters); // Καλέστε τη συνάρτηση ξανά μετά από το χρονικό διάστημα
         }
-        screenText.appendChild(myspan);
+        element.appendChild(myspan);
+    }
+
+    addNextChar(); // Εκκίνηση του animation
+}
+
+
+
+function animateTextWithoutCursor(text, delayBetweenLetters = 20, initialText = "") {
+    SignOutMessage.innerHTML = initialText // Καθαρίστε το κείμενο
+    let index = 0;
+
+
+    function addNextChar() {
+        if (index < text.length) {
+            if (text[index] === '\n') {
+                SignOutMessage.innerHTML += '<br>'; // Προσθέστε <br> για αλλαγή γραμμής
+            } else {
+                SignOutMessage.innerHTML += text[index] ;
+            }
+            index++;
+            setTimeout(addNextChar, delayBetweenLetters); // Καλέστε τη συνάρτηση ξανά μετά από το χρονικό διάστημα
+        }
     }
 
     addNextChar(); // Εκκίνηση του animation
@@ -337,6 +458,19 @@ async function RevealButtons(buttons, startingtime){
         time += 200;
     }
 
+}
+
+async function LoadBar(element, time=1500){
+    return element.animate([
+        {width: "0%"},
+        {width: "100%"}
+    ],{
+        duration: time,
+        easing: "ease-in-out",
+        delay: 0,
+        iterations: 1,
+        fill: 'forwards'
+    })
 }
 
 
